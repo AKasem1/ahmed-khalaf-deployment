5377ac54c418257d6b5d88ae756988198f966741
AKasem1 <abdelrahmankasem7@gmail.com>
2024-09-05 19:46:31 +0300

    adding side menu bar

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -5,46 +5,61 @@ import logo from '../assets/logo.png';
 import {useDispatch} from 'react-redux';
 
 export const Navbar = () => {
-  const location = useLocation();
-  const [dropdownVisible, setDropdownVisible] = useState(false);
+  const [sidebarVisible, setSidebarVisible] = useState(false);
   const dispatch = useDispatch();
 
-  const toggleDropdown = () => {
-    setDropdownVisible(!dropdownVisible);
+  const toggleSidebar = () => {
+    setSidebarVisible(!sidebarVisible);
   };
-
-
 
   return (
     <header className="header">
       <div className="logo">
         <RouterLink to='/' className="logo-link">
-          <img src={logo} alt="منصة زين التعليمية" />
+          <img src={logo} alt="منصة الحكيم التعليمية" />
         </RouterLink>
       </div>
-        <nav className="nav">
-          <ScrollLink to="footer" smooth={true} duration={500} className='link'>
-            تواصل معنا
-          </ScrollLink>
-          <ScrollLink to="aboutus" smooth={true} duration={500} className='link'>
-            من نحن
-          </ScrollLink>
-          <ScrollLink to="courses" smooth={true} duration={500} className='link'>
-            الكورسات
-          </ScrollLink>
-          <ScrollLink to="grades" smooth={true} duration={500} className='link'>
-            المراحل الدراسية
-          </ScrollLink>
-          <ScrollLink to="home" smooth={true} duration={500} className='link'>
-            الرئيسية
-          </ScrollLink>
-        </nav>
-        <button>
-          تسجيل دخول
-          <div class="arrow-wrapper">
-              <div class="arrow"></div>
+      <nav className="nav">
+        <ScrollLink to="footer" smooth={true} duration={500} className='link'>
+          تواصل معنا
+        </ScrollLink>
+        <ScrollLink to="aboutus" smooth={true} duration={500} className='link'>
+          من نحن
+        </ScrollLink>
+        <ScrollLink to="courses" smooth={true} duration={500} className='link'>
+          الكورسات
+        </ScrollLink>
+        <ScrollLink to="grades" smooth={true} duration={500} className='link'>
+          المراحل الدراسية
+        </ScrollLink>
+        <ScrollLink to="home" smooth={true} duration={500} className='link'>
+          الرئيسية
+        </ScrollLink>
+      </nav>
+
+      <div className="menu-icon" onClick={toggleSidebar}>
+        ☰
+      </div>
+
+      <div className={`sidebar ${sidebarVisible ? 'active' : ''}`}>
+        <div className="close-btn" onClick={toggleSidebar}>
+          &times;
+        </div>
+        <img src={logo} alt="منصة الحكيم" className="sidebar-logo"/>
+        <hr />
+        <button className="sidebar-button">
+          حساب جديد
+          <div className="arrow-wrapper">
+            <div className="arrow"></div>
           </div>
-      </button>
+        </button>
+        <button className="sidebar-button">
+          تسجيل دخول
+          <div className="arrow-wrapper">
+            <div className="arrow"></div>
+          </div>
+        </button>
+      </div>
     </header>
   );
 };
